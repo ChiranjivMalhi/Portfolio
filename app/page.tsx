48efffb0aa8dad8cffe157448087ca84fe2b3f76
Chiranjiv Singh Malhi <50653577+ChiranjivMalhi@users.noreply.github.com>
2024-10-20 19:59:21 +0530

    added loading page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,42 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
 import { FloatingNav } from "@/components/FloatingNav";
 import Hero from "@/components/Hero";
-import {FaBeer} from "react-icons/fa";
 import Image from "next/image";
 import Grid from "@/components/Grid";
 import DemoReel from "@/components/DemoReel";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Footer from "@/components/Footer";
-import { Carousel } from "@/components/ui/apple-cards-carousel";
-import { AppleCardsCarouselDemo } from "@/components/Carousel";
-import { BoxesCore } from "@/components/ui/background-boxes";
 import About from "@/components/About";
-import { Suspense } from "react";
 import Loading from "@/components/Loading";
 
-
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // 10 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <main className="relative bg-beige-100 flex justify-center overflow-x-hidden items-center flex-col mx-auto sm:px-10 px-5">
-      
       <div className="max-w-7xl w-full">
-        
-        <FloatingNav navItems={navItems}/>
-        <Hero/>
-        <About/>
-        <div className="hidden lg:block"><Grid/></div>
-        <DemoReel/>
-        <RecentProjects/>
-        <Footer/>
-        
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <About />
+        <div className="hidden lg:block"><Grid /></div>
+        <DemoReel />
+        <RecentProjects />
+        <Footer />
       </div>
     </main>
   );
--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 import { InfinitySpin } from 'react-loader-spinner'
 
 const Loading = () => {
-  return (
-        <div>
-        <InfinitySpin
-        width="200"
-        color="#4fa94d"
-        />
+    return (
+        <div className='w-screen flex justify-center items-center h-screen'>
+            <InfinitySpin
+                width="200"
+                color="#4C009C"
+            />
         </div>
-  )
+    )
 }
 
 export default Loading
